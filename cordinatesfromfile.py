7e99c5f601d9979c5483a7ca3806340deeb49bd0
thekaranjit <thekaranjit@gmail.com>
2018-08-09 03:18:55 +0400

    till elevation and markers

--- cordinatesfromfile.py
+++ cordinatesfromfile.py
@@ -1,5 +1,49 @@
 import pandas
+import folium
 
 data = pandas.read_csv("volcano.txt")
 
-print(data)
+lat = list(data["LAT"])
+lon = list(data["LON"])
+elev = list(data["ELEV"])
+
+def color_producer(elevation):
+    if elevation < 1000:
+        return 'green'
+    elif 1000 <= elevation < 3000:
+        return'orange'
+    else:
+        return 'red'
+
+map = folium.Map(
+    location=[38.58, -99.09],
+    zoom_start=6,
+    tiles="Mapbox Bright")
+
+fg = folium.FeatureGroup(name="My Map")
+
+marker_color = ''
+
+
+
+
+for lt, ln, el in zip(lat, lon, elev):
+    fg.add_child(folium.Marker(
+        location=[lt, ln],
+        popup="Elevation is " + str(el),
+        icon=folium.Icon(color=color_producer(el)
+        )))
+
+
+fg.add_child(folium.GeoJson())
+
+
+
+map.add_child(fg)
+
+
+map.save("Map2.html")
+
+
+
+
